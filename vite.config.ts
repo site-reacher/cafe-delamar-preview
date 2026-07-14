530de7ca050344bfd4750c5913d7ed1ed8aa4d67
site-reacher <luck057@proton.me>
2026-07-14 19:30:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Required because this preview is hosted inside a GitHub repository path.
+  base: '/cafe-delamar-preview/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
